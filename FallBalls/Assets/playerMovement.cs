670a2c0b9d4aabd739d6e561b9ce9c148a5ee9a5
Juff2020 <jeffreybtennant@gmail.com>
2020-06-12 15:31:10 +0800

    demo version 0.1

---
+++ FallBalls/Assets/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody2D;
+    public float horizontalMove;
+    public float jumpForce;
+    public bool isJumping;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        rigidbody2D.AddForce(new Vector2(horizontalMove, 0f), ForceMode2D.Force);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+    }
+}
